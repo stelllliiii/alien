ef5f7f3d9af22c973e20025deb0369ff2c73f786
_live_ <_live@example.com>
2023-12-02 15:38:52 +0800

    ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,25 @@
 import sys
 import pygame
+from settings import Settings
 
 class  AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.bg_color = (230,230,230)
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
+            #重绘屏幕
+            self.screen.fill(self.settings.bg_color)
             pygame.display.filp()
 
 if __name__== ' __main__':
