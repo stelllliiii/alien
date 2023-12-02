d39cec898c9c34e02e9d8d54d2ba0f10d90a51de
_live_ <_live@example.com>
2023-12-02 17:42:06 +0800

    设置移动速度

--- ship.py
+++ ship.py
@@ -3,6 +3,7 @@ import pygame
 class Ship:
     def __init__(self,ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/ship.bmp')
@@ -10,15 +11,21 @@ class Ship:
 
         self.rect.midbottom = self.screen_rect.midbottom
     
+        #x 储存小数
+        self.x = float(self.rect.x)
+        
+        #移动
         self.moving_right = False
         self.moving_left = False
 
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image,self.rect)
