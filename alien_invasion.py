f39a1c5f745fa2529437f851f57881aed942a182
_live_ <_live@example.com>
2023-12-02 17:28:26 +0800

    左右移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,14 +15,34 @@ class  AlienInvasion:
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        #响应按键和鼠标事件
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            #重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
 
-            pygame.display.flip()
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                      self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                      self.ship.moving_left = False
+
+    def _update_screen(self):
+        #重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
